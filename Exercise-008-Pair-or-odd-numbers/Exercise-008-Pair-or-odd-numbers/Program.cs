b68a4ba9273b1b26497c98a7d4959c4f164503d5
Abrão Astério Junior <abcorinthians@gmail.com>
2020-11-30 14:22:42 -0300

    Exercise 8: Pair or odd numbers

---
+++ Exercise-008-Pair-or-odd-numbers/Exercise-008-Pair-or-odd-numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_008_Pair_or_odd_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read an integer and tell if the number is pair or odd.*/
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0)
+                Console.WriteLine("PAIR");
+            else
+                Console.WriteLine("ODD");
+
+        }
+    }
+}
