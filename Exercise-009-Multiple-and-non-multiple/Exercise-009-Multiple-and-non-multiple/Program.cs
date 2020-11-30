b158610ccfe32a284b208e66df934093933a638c
Abrão Astério Junior <abcorinthians@gmail.com>
2020-11-30 14:25:58 -0300

    Exercise 9: Multiple and non-multiple numbers

---
+++ Exercise-009-Multiple-and-non-multiple/Exercise-009-Multiple-and-non-multiple/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise_009_Multiple_and_non_multiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Read 2 integer values (A and B). Then, the program should display the message "Are multiple" or "Are not multiple",
+             * indicating whether the values read are multiple with each other. Attention: numbers must be entered 
+             * ascending or descending order.*/
+
+            string[] vet = Console.ReadLine().Split(' ');
+            int A = int.Parse(vet[0]);
+            int B = int.Parse(vet[1]);
+
+            if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("They are multiple!");
+            }
+            else
+            {
+                Console.WriteLine("They're not multiple!");
+            }
+        }
+    }
+}
