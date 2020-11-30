2b467714dd9c35eaafced93aa6518124abdb4220
Abrão Astério Junior <abcorinthians@gmail.com>
2020-11-30 14:19:00 -0300

    Exercise 7: Negative and non-negative numbers

---
+++ Exercise-007-Positive-or-negative-numbers/Exercise-007-Positive-or-negative-numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_007_Positive_or_negative_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read an integer and tell if that number is negative or not.*/
+            int x = int.Parse(Console.ReadLine());
+
+            if (x >= 0)
+                Console.WriteLine("NON-NEGATIVE");
+            else
+                Console.WriteLine("NEGATIVE");
+        }
+    }
+}
