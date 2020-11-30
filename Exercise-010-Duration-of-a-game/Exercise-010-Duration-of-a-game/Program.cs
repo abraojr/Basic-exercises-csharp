c3c7f902f17308d6bf04dc9e766a808a1b3b3dc1
Abrão Astério Junior <abcorinthians@gmail.com>
2020-11-30 14:29:46 -0300

    Exercise 10: Duration of a game

---
+++ Exercise-010-Duration-of-a-game/Exercise-010-Duration-of-a-game/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise_010_Duration_of_a_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Read the start and end time of a game. Then calculate the duration of the game, knowing that it can 
+             * start on one day and end on another, having a minimum duration of 1 hour and maximum of 24 hours.*/
+
+            string[] vet = Console.ReadLine().Split(' ');
+            int A = int.Parse(vet[0]);
+            int B = int.Parse(vet[1]);
+
+            int totDuration;
+
+            if (A > B)
+            {
+                totDuration = 24 - A + B;
+                Console.WriteLine("THE GAME LASTED {0} HOURS", totDuration);
+            }
+            else if (B > A)
+            {
+                totDuration = B - A;
+                Console.WriteLine("THE GAME LASTED {0} HOURS", totDuration);
+            }
+            else
+            {
+                Console.WriteLine("THE GAME LASTED 24 HOURS");
+            }
+        }
+    }
+}
