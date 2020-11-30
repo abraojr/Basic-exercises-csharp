cdd4c026867cbc34bc70a89a2335956613ef748d
Abrão Astério Junior <abcorinthians@gmail.com>
2020-11-30 12:47:50 -0300

    Exercise 3: Product of difference

---
+++ Exercise-003-Product-of-difference/Exercise-003-Product-of-difference/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise_003_Product_of_difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read four integer values A, B, C and D. Then calculate and show the product difference of A and B by
+             * the product of C and D according to the formula: DIFFERENCE = (A * B - C * D).*/
+            int a, b, c, d, dif;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+
+            dif = (a * b - c * d);
+            Console.WriteLine("DIFFERENCE: " + dif);
+        }
+    }
+}
