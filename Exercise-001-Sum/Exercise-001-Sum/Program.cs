311ad7f36fd543f8ff312188542d861d3b0c063e
Abrão Astério Junior <abcorinthians@gmail.com>
2020-11-30 12:39:27 -0300

    Exercise 1: Sum of two integer numbers

---
+++ Exercise-001-Sum/Exercise-001-Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_001_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program that sums two integer numbers*/
+
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            int sum = a + b;
+
+            Console.WriteLine("SUM:" + sum);
+        }
+    }
+}
