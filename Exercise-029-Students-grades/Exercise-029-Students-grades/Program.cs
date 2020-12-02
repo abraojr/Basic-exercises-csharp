0b819161f99de7ea59563d0193cf7d55017c48df
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-02 20:27:09 -0300

    Exercise 29: Student's grades

---
+++ Exercise-029-Students-grades/Exercise-029-Students-grades/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_029_Students_grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read a student's name and the three grades he obtained in the three quarters of the year 
+             *(first quarter is worth 30 and the second and third are worth 35 each). At the end, show the student's final grade in the year.
+             *Also say whether the student was APROVED or REPROVED and, if its reproved, how many points are left for the student to obtain
+             *the minimum to pass (which is 60 points).*/
+
+            Student x;
+            x = new Student();
+
+            Console.Write("Student's name: ");
+            x.Name = Console.ReadLine();
+            Console.WriteLine("Enter the student's three grades: ");
+            x.Grade1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Grade2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Grade3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double finalGrade = x.FinalGrade();
+
+            Console.WriteLine("FINAL GRADE : " + x);
+
+            if (x.Aproved())
+            {
+                Console.WriteLine("APROVED");
+            }
+            else
+            {
+                Console.WriteLine("REPROVED");
+                Console.WriteLine("LACKED: " + x.RemainingGrade().ToString("F2", CultureInfo.InvariantCulture) + " POINTS");
+            }
+
+        }
+    }
+}
---
+++ Exercise-029-Students-grades/Exercise-029-Students-grades/Student.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace Exercise_029_Students_grades
+{
+    class Student
+    {
+        public string Name;
+        public double Grade1;
+        public double Grade2;
+        public double Grade3;
+
+        public double FinalGrade()
+        {
+            return Grade1 + Grade2 + Grade3;
+        }
+        public bool Aproved()
+        {
+            if (FinalGrade() >= 60.0)
+                return true;
+            else
+                return false;
+        }
+        public double RemainingGrade()
+        {
+            if (Aproved())
+                return 0.0;
+            else
+                return 60 - FinalGrade();
+        }
+        public override string ToString()
+        {
+            return FinalGrade().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
