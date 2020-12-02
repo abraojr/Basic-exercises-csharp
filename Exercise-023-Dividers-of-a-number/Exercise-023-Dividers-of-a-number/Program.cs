88e782bdb9b2396efbdddde5057881178922fb46
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-02 02:12:42 -0300

    Exercise 23: All divisors of a number.

---
+++ Exercise-023-Dividers-of-a-number/Exercise-023-Dividers-of-a-number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_023_Dividers_of_a_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Read an integer N and calculate all of its divisors.*/
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (N % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
