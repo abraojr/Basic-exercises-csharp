aafc15bf3029964a4f21bc9c9a7e189ce53ce10c
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-02 19:57:16 -0300

    Exercise 28: Raising an employee's salary

---
+++ Exercise-028-Raising-an-employees-salary/Exercise-028-Raising-an-employees-salary/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Exercise_028_Raising_an_employees_salary
+{
+    class Employee
+    {
+        public string Name;
+        public double GrossSalary;
+        public double Tax;
+
+        public double NetSalary()
+        {
+            return GrossSalary - Tax;
+        }
+        public void RaiseSalary(double percentage)
+        {
+            GrossSalary += GrossSalary * (percentage / 100);
+        }
+        public override string ToString()
+        {
+            return Name + " , U$ " + NetSalary().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercise-028-Raising-an-employees-salary/Exercise-028-Raising-an-employees-salary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_028_Raising_an_employees_salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read an employee's data (name, gross salary and tax). Then, show the employee data (name and net salary).
+             * Then, increase the employee's salary based on a given percentage (only the gross salary is affected by the percentage) and
+             * show the employee's data again.*/
+
+            Employee x;
+            x = new Employee();
+
+            Console.Write("Name: ");
+            x.Name = Console.ReadLine();
+            Console.Write("Gross salary : U$ ");
+            x.GrossSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Tax: U$ ");
+            x.Tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.WriteLine("Employee: " + x);
+            Console.WriteLine();
+
+            Console.Write("Enter the percentage to raise the salary: ");
+            double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.RaiseSalary(percentage);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated data: " + x);
+        }
+    }
+}
