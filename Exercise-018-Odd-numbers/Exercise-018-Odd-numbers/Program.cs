5052a6f65cd69016f1a97fe3c438747a6c6fd539
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-01 22:23:36 -0300

    Exercise 18: Odd numbers from 1 to 1000

---
+++ Exercise-018-Odd-numbers/Exercise-018-Odd-numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_018_Odd_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Read an integer value X (1 <= X <= 1000). Then show the odd numbers from 1 to X, one value per line, including the 
+             * X, if applicable.*/
+
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= x; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
