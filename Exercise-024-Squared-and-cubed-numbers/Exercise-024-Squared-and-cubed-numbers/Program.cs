261b84c886672d16b1fd93dda8833cafe4dac4d6
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-02 02:27:21 -0300

    Exercise 24: Squared and cubed numbers

---
+++ Exercise-024-Squared-and-cubed-numbers/Exercise-024-Squared-and-cubed-numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_024_Squared_and_cubed_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read a positive integer N. The program should then show on the screen N lines,
+             * starting from 1 to N. For each line, show the line number, then the square and the value cube.*/
+
+
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= N; i++)
+            {
+                double n1 = Math.Pow(i, 2.0);
+                double n2 = Math.Pow(i, 3.0);
+                Console.WriteLine("{0} {1} {2}", i, n1, n2);
+            }
+        }
+    }
+}
