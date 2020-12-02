408d3b9293062c7241b074a8552af20a8304050c
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-02 17:19:18 -0300

    Exercise 25: Creating a class Person

---
+++ Exercise-025-Class-person/Exercise-025-Class-person/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Exercise_025_Class_person
+{
+    class Person
+    {
+        public string Name;
+        public int Age;
+    }
+}
---
+++ Exercise-025-Class-person/Exercise-025-Class-person/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise_025_Class_person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read the data of two people, then show the name of the oldest person.*/
+
+            Person p1, p2;
+            p1 = new Person();
+            p2 = new Person();
+
+            Console.WriteLine("First person's data: ");
+            Console.Write("Name: ");
+            p1.Name = Console.ReadLine();
+            Console.Write("Age: ");
+            p1.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Second person's data:");
+            Console.Write("Name: ");
+            p2.Name = Console.ReadLine();
+            Console.Write("Age: ");
+            p2.Age = int.Parse(Console.ReadLine());
+
+            if (p1.Age > p2.Age)
+                Console.WriteLine("The oldest person is: " + p1.Name);
+            else
+                Console.WriteLine("The oldest person is: " + p2.Name);
+        }
+    }
+}
