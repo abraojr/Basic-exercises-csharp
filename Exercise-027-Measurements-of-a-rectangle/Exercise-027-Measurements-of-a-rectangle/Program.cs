72d5a901442a5f7e397e549424aa6cfdf81ca678
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-02 19:38:26 -0300

    Exercise 27: Area, perimeter and diagonal of a rectangle, usingclass and methods

---
+++ Exercise-027-Measurements-of-a-rectangle/Exercise-027-Measurements-of-a-rectangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_027_Measurements_of_a_rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Make a program to read the values of the width and height of a rectangle. Then show the value of your area,
+             * perimeter and diagonal on the screen.*/
+
+            Rectangle A;
+            A = new Rectangle();
+
+
+            Console.WriteLine("Enter the width and height of the rectangle: ");
+            A.Widht = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            A.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaRect = A.Area();
+            double perRect = A.Perimeter();
+            double diagRect = A.Diagonal();
+
+            Console.WriteLine("AREA: " + areaRect.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRER: " + perRect.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL: " + diagRect.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Exercise-027-Measurements-of-a-rectangle/Exercise-027-Measurements-of-a-rectangle/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_027_Measurements_of_a_rectangle
+{
+    class Rectangle
+    {
+        public double Widht;
+        public double Height;
+
+        public double Area()
+        {
+            return Widht * Height;
+        }
+        public double Perimeter()
+        {
+            return 2 * Widht + 2 * Height;
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Widht, 2.0) + Math.Pow(Height, 2.0));
+        }
+
+    }
+}
