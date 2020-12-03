6252eea8faddc1950163b5ccffde8119c32a575e
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-02 21:53:35 -0300

    Exercise 30: Currency Converter

---
+++ Exercise-030-Currency-converter/Exercise-030-Currency-converter/CurrencyConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise_030_Currency_converter
+{
+    class CurrencyConverter
+    {
+        public static double Iof = 6.0;
+        public static double Convert(double quotation, double dollar)
+        {
+            double Total = quotation * dollar;
+            return Total += Total * (Iof / 100.0);
+        }
+    }
+}
---
+++ Exercise-030-Currency-converter/Exercise-030-Currency-converter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_030_Currency_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read the dollar quotation, and then a dollar amount to be bought by a person in Reais. 
+             *Inform how many Reais the person will pay for the dollars, also considering that the person will have to pay 6% IOF
+             *on the dollar amount*/
+
+            Console.Write("What is the dollar quote? ");
+            double quotation = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("How many dollars will you buy? U$ ");
+            double dollar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = CurrencyConverter.Convert(quotation, dollar);
+
+            Console.WriteLine("Amount to be paid in Reais: R$ " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
