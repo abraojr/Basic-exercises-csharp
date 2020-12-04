3b8c9ab23a2fb3deeb0c5f6453b6a32192a99087
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-04 01:41:18 -0300

    Exercise 32 : Renting a pension - Added exercise statement

--- Exercise-032-Renting-a-pension/Exercise-032-Renting-a-pension/Program.cs
+++ Exercise-032-Renting-a-pension/Exercise-032-Renting-a-pension/Program.cs
@@ -6,6 +6,13 @@ namespace Exercise_032_Renting_a_pension
     {
         static void Main(string[] args)
         {
+            /* The owner of a boarding house has ten rooms to rent for students, these rooms being identified by the numbers 0 to 9.When 
+             *a student wants to rent a room, one must register the name and email of this student.Make a program that starts with all ten 
+             *empty rooms, and then read an N amount representing the number of students that will rent rooms (N can be from 1 to 10).Then, 
+             *register the N students' rent. For each rental registration, inform the name and email of the student, as well as which of the 
+             *rooms he chose (from 0 to 9). Suppose a vacant room is chosen.At the end, your program must print a report of all occupations of
+             *the boarding house, in order of room*/
+
             Student[] vect = new Student[10];
             Console.Write("How many rooms will be rented? ");
             int n = int.Parse(Console.ReadLine());
