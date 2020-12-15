752a1bfdcc078c33b285d80e2444943e9fd0e19f
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-15 05:26:31 -0300

    Exercise 46: Reading employee files

---
+++ Exercise-046-Reading-employee-files/Exercise-046-Reading-employee-files/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise_046_Reading_employee_files.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
---
+++ Exercise-046-Reading-employee-files/Exercise-046-Reading-employee-files/Program.cs
@@ -0,0 +1,58 @@
+﻿using Exercise_046_Reading_employee_files.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Exercise_046_Reading_employee_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read the data (name, email and salary) of employees from a file in .csv format. Then show, in alphabetical order, the email 
+             *of employees whose salary is higher than a given value provided by the user.Show also the sum of salaries of employees whose name starts with 
+             *the letter 'M'.*/
+
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+            Console.Write("Enter salary: ");
+            double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            List<Employee> list = new List<Employee>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                        list.Add(new Employee(name, email, salary));
+                    }
+
+                    var emails = list.Where(obj => obj.Salary > limit).OrderBy(obj => obj.Email).Select(obj => obj.Email);
+
+                    var sum = list.Where(obj => obj.Name[0] == 'M').Sum(obj => obj.Salary);
+
+                    Console.WriteLine("Email of people whose salary is more than " + limit.ToString("F2", CultureInfo.InvariantCulture));
+                    foreach (string email in emails)
+                    {
+                        Console.WriteLine(email);
+                    }
+
+                    Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
